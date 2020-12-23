3813ecca880e89761af2721b9669a57960691d82
Reevan <ree1dsouza@gmail.com>
2020-12-23 21:36:08 +0530

    i have added few more api end points

--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ from flask_restful import Api,Resource;
 from flask_cors import CORS;
 import mysql.connector
 from mysql.connector import cursor;
+import json
 
 db = mysql.connector.connect(
     host="localhost",
@@ -39,6 +40,7 @@ class Disease(Resource):
         cursor.close();
         return record;
 
+
 class Symptoms(Resource):
     def get(self):
         disease = request.args.get("disease");
@@ -56,14 +58,108 @@ class Symptoms(Resource):
         cursor = db.cursor();
         cursor.execute(query%(disease));
         symptoms = cursor.fetchall();
+        cursor.close();
         result =[]
         for i in symptoms:
             result.append(i[0]);
         return result;
-        
+    
 
-api.add_resource(Disease,"/disease");
-api.add_resource(Symptoms,"/symptoms")
+
+class SymptomsAndParts(Resource):
+    def get(self):
+        disease = request.args.get("disease");
+        print("disease names are = %s"%(disease));
+        query = """
+             select sName,sPart from
+             disease d,diseaseSymptoms ds,symptoms s
+             where d.dId = ds.dId
+             and ds.sId = s.sId
+             and dName = "%s";
+        """
+        cursor = db.cursor();
+        cursor.execute(query%(disease));
+        result = cursor.fetchall();
+        cursor.close();
+        return json.dumps(result);
+
+class Medicine(Resource):
+    def get(self):
+        query = """
+              select mName,modeOfAdministration
+              from medicine
+              where mId in (
+	                select mId 
+                    from treatment t,disease d
+                    where d.dId = t.dId
+                    and dName = "%s"
+                );
+  
+        """
+        disease = request.args.get("disease");
+        cursor = db.cursor();
+        cursor.execute(query%(disease));
+        result = cursor.fetchall();
+        cursor.close();
+        return result;
+
+class SimilarMedicines(Resource):
+    def get(self):
+        query = """
+            select mName,modeOfAdministration
+            from medicine
+            where mId in(
+	            select similar
+	            from  similarMedicine
+	            where mId = ( select mId from medicine where mName = "%s") 
+            );
+        """
+        name = request.args.get("medicine");
+        cursor = db.cursor();
+        cursor.execute(query%(name));
+        result = cursor.fetchall();
+        cursor.close();
+        return result;
+
+class Chemicals(Resource):
+    def get(self):
+        query = """
+            select cName
+            from chemicals
+            where cId in(
+	            select cId
+	            from medicineContents
+	            where mId = (select mId from medicine where mName = "%s")
+            );
+        """
+        medicine = request.args.get("medicine");
+        cursor = db.cursor();
+        cursor.execute(query%(medicine));
+        result = cursor.fetchall();
+        return result;
+
+class MedicineWithChemicals(Resource):
+    def get(self):
+        query = """
+            select mName
+            from medicine m,chemicals c,medicineContents cc
+            where m.mId = cc.mId and c.cId = cc.cId 
+            and c.cName = "%s";
+         """
+        cursor = db.cursor();
+        chemical = request.args.get("chemical");
+        cursor.execute(query%(chemical));
+        result = cursor.fetchall();
+        cursor.close();
+        return result;
+
+api.add_resource(Disease,"/disease/");
+api.add_resource(Symptoms,"/symptoms/")
+api.add_resource(SymptomsAndParts,"/symptoms-and-parts/");
+api.add_resource(Medicine,"/medicines/");
+api.add_resource(SimilarMedicines,"/similar-medicines/");
+api.add_resource(Chemicals,"/show-chemicals/");
+api.add_resource(MedicineWithChemicals,"/medicine-with-chemical/");
 
 if __name__ =="__main__":
     app.run(debug=True);
